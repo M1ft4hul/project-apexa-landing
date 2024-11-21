cb7750569025970d186d8747ff431e9e6862e61a
Abd. Majid Hamid <abd.majidehamide@gmail.com>
2024-11-21 12:13:52 +0800

    Layanan Unggulan 20% progres

--- app/componen/keunggulan/index.tsx
+++ app/componen/keunggulan/index.tsx
@@ -12,94 +12,126 @@ const Fitur = () => {
       triggerOnce={true}>
       <div className='mx-auto max-w-9xl py-16 px-6 bg-[#F2F7FD]'>
         <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-y-20 gap-x-5'>
-          <div className='flex justify-center items-center space-x-6'>
-            <div className='flex justify-center '>
-              <img
-                alt='/banner/dental.png'
-                loading='lazy'
-                width={65}
-                height={65}
-                decoding='async'
-                data-nimg={1}
-                style={{ color: "transparent" }}
-                src='/banner/dental.png'
-              />
+          {/* data 1 */}
+          <Fade
+            direction={"up"}
+            delay={400}
+            cascade
+            damping={1e-1}
+            triggerOnce={true}>
+            <div className='flex justify-center items-center space-x-6'>
+              <div className='flex justify-center '>
+                <img
+                  alt='/banner/dental.png'
+                  loading='lazy'
+                  width={65}
+                  height={65}
+                  decoding='async'
+                  data-nimg={1}
+                  style={{ color: "transparent" }}
+                  src='/banner/dental.png'
+                />
+              </div>
+              <div className='flex flex-col'>
+                <h2 className='text-4xl lg:text-5xl text-black font-normal text-star mt-5'>
+                  200+
+                </h2>
+                <h3 className='text-1xl text-black font-normal text-center lg:mt-2'>
+                  HAPPY PATIENTS
+                </h3>
+              </div>
             </div>
-            <div className='flex flex-col'>
-              <h2 className='text-4xl lg:text-5xl text-black font-normal text-star mt-5'>
-                200+
-              </h2>
-              <h3 className='text-1xl text-black font-normal text-center lg:mt-2'>
-                HAPPY PATIENTS
-              </h3>
+          </Fade>
+          {/* data 2 */}
+          <Fade
+            direction={"up"}
+            delay={400}
+            cascade
+            damping={1e-1}
+            triggerOnce={true}>
+            <div className='flex justify-center items-center space-x-6'>
+              <div className='flex justify-center '>
+                <img
+                  alt='/banner/saved.png'
+                  loading='lazy'
+                  width={65}
+                  height={65}
+                  decoding='async'
+                  data-nimg={1}
+                  style={{ color: "transparent" }}
+                  src='/banner/saved.png'
+                />
+              </div>
+              <div className='flex flex-col'>
+                <h2 className='text-4xl lg:text-5xl text-black font-normal text-star mt-5'>
+                  20+
+                </h2>
+                <h3 className='text-1xl text-black font-normal text-center lg:mt-2'>
+                  SAVED HEARTS
+                </h3>
+              </div>
             </div>
-          </div>
-          <div className='flex justify-center items-center space-x-6'>
-            <div className='flex justify-center '>
-              <img
-                alt='/banner/saved.png'
-                loading='lazy'
-                width={65}
-                height={65}
-                decoding='async'
-                data-nimg={1}
-                style={{ color: "transparent" }}
-                src='/banner/saved.png'
-              />
+          </Fade>
+          {/* data 3 */}
+          <Fade
+            direction={"up"}
+            delay={400}
+            cascade
+            damping={1e-1}
+            triggerOnce={true}>
+            <div className='flex justify-center items-center space-x-6'>
+              <div className='flex justify-center '>
+                <img
+                  alt='/banner/phone.png'
+                  loading='lazy'
+                  width={55}
+                  height={55}
+                  decoding='async'
+                  data-nimg={1}
+                  style={{ color: "transparent" }}
+                  src='/banner/phone.png'
+                />
+              </div>
+              <div className='flex flex-col'>
+                <h2 className='text-4xl lg:text-5xl text-black font-normal text-star mt-5'>
+                  10k+
+                </h2>
+                <h3 className='text-1xl text-black font-normal text-center lg:mt-2'>
+                  EXPERT DOCTORS
+                </h3>
+              </div>
             </div>
-            <div className='flex flex-col'>
-              <h2 className='text-4xl lg:text-5xl text-black font-normal text-star mt-5'>
-                20+
-              </h2>
-              <h3 className='text-1xl text-black font-normal text-center lg:mt-2'>
-                SAVED HEARTS
-              </h3>
+          </Fade>
+          {/* data 4 */}
+          <Fade
+            direction={"up"}
+            delay={400}
+            cascade
+            damping={1e-1}
+            triggerOnce={true}>
+            <div className='flex justify-center items-center space-x-6'>
+              <div className='flex justify-center '>
+                <img
+                  alt='/banner/files.png'
+                  loading='lazy'
+                  width={60}
+                  height={60}
+                  decoding='async'
+                  data-nimg={1}
+                  style={{ color: "transparent" }}
+                  src='/banner/files.png'
+                />
+              </div>
+              <div className='flex flex-col'>
+                <h2 className='text-4xl lg:text-5xl text-black font-normal text-star mt-5'>
+                  900+
+                </h2>
+                <h3 className='text-1xl text-black font-normal text-center lg:mt-2'>
+                  SERENITY WORK
+                </h3>
+              </div>
             </div>
-          </div>
-          <div className='flex justify-center items-center space-x-6'>
-            <div className='flex justify-center '>
-              <img
-                alt='/banner/phone.png'
-                loading='lazy'
-                width={55}
-                height={55}
-                decoding='async'
-                data-nimg={1}
-                style={{ color: "transparent" }}
-                src='/banner/phone.png'
-              />
-            </div>
-            <div className='flex flex-col'>
-              <h2 className='text-4xl lg:text-5xl text-black font-normal text-star mt-5'>
-                10k+
-              </h2>
-              <h3 className='text-1xl text-black font-normal text-center lg:mt-2'>
-                EXPERT DOCTORS
-              </h3>
-            </div>
-          </div>
-          <div className='flex justify-center items-center space-x-6'>
-            <div className='flex justify-center '>
-              <img
-                alt='/banner/files.png'
-                loading='lazy'
-                width={60}
-                height={60}
-                decoding='async'
-                data-nimg={1}
-                style={{ color: "transparent" }}
-                src='/banner/files.png'
-              />
-            </div>
-            <div className='flex flex-col'>
-              <h2 className='text-4xl lg:text-5xl text-black font-normal text-star mt-5'>
-                900+
-              </h2>
-              <h3 className='text-1xl text-black font-normal text-center lg:mt-2'>
-                SERENITY WORK
-              </h3>
-            </div>
-          </div>
+          </Fade>
         </div>
       </div>
     </Fade>
--- app/componen/layanan/index.tsx
+++ app/componen/layanan/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { Fade } from "react-awesome-reveal";
 
@@ -17,6 +17,23 @@ const Layanan = () => {
               Layanan Unggulan
             </h3>
           </Fade>
+          {/* layanan */}
+          <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow transition duration-300 hover:border-blue-500 cursor-pointer'>
+            <a href='#'>
+              <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
+                Noteworthy technology acquisitions 2021
+              </h5>
+            </a>
+            <p className='mb-3 font-normal text-gray-700'>
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <a
+              href='#'
+              className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300'>
+              Read more
+            </a>
+          </div>
         </div>
       </div>
     </div>
