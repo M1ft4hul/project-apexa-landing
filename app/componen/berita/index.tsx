f35cb9952d85d7cfa94bbdea2061b22bb7215734
Abd. Majid Hamid <abd.majidehamide@gmail.com>
2024-11-21 14:58:08 +0800

    berita layout 2%  progres

---
+++ app/componen/berita/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Fade } from "react-awesome-reveal";
+
+const News = () => {
+  return (
+    <div>
+      <div className='mx-auto max-w-7xl layanan px-6'>
+        <div className='text-center'>
+          <Fade
+            direction={"up"}
+            delay={400}
+            cascade
+            damping={1e-1}
+            triggerOnce={true}>
+            <h3 className='text-3xl lg:text-5xl font-normal mb-12 text-lightgrey md:4px lg:text-center'>
+              Layanan Unggulan
+            </h3>
+          </Fade>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default News;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Banner from "./componen/banners/index";
 import Work from "./componen/penawaran";
 import Fitur from "./componen/keunggulan";
 import Layanan from "./componen/layanan";
+import News from "./componen/berita";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <Work />
         <Fitur />
         <Layanan />
+        <News />
       </main>
     </>
   );
