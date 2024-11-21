84b2fa1834b22b778eb76518938a55f6db13f7ff
Abd. Majid Hamid <abd.majidehamide@gmail.com>
2024-11-21 12:28:49 +0800

    Layanan Unggulan 50% progres

--- app/componen/layanan/index.tsx
+++ app/componen/layanan/index.tsx
@@ -18,22 +18,62 @@ const Layanan = () => {
             </h3>
           </Fade>
           {/* layanan */}
-          <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow transition duration-300 hover:border-blue-500 cursor-pointer'>
-            <a href='#'>
-              <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
-                Noteworthy technology acquisitions 2021
-              </h5>
-            </a>
-            <p className='mb-3 font-normal text-gray-700'>
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href='#'
-              className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300'>
-              Read more
-            </a>
+          <div className='flex justify-center'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+              {/* layanan 1 */}
+              <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow transition duration-300 hover:border-blue-500 cursor-pointer'>
+                <a href='#'>
+                  <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
+                    Noteworthy technology acquisitions 2021
+                  </h5>
+                </a>
+                <p className='mb-3 font-normal text-gray-700'>
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <a
+                  href='#'
+                  className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300'>
+                  Read more
+                </a>
+              </div>
+              {/* layanan 2 */}
+              <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow transition duration-300 hover:border-blue-500 cursor-pointer'>
+                <a href='#'>
+                  <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
+                    Noteworthy technology acquisitions 2021
+                  </h5>
+                </a>
+                <p className='mb-3 font-normal text-gray-700'>
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <a
+                  href='#'
+                  className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300'>
+                  Read more
+                </a>
+              </div>
+              {/* layanan 3 */}
+              <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow transition duration-300 hover:border-blue-500 cursor-pointer'>
+                <a href='#'>
+                  <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
+                    Noteworthy technology acquisitions 2021
+                  </h5>
+                </a>
+                <p className='mb-3 font-normal text-gray-700'>
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <a
+                  href='#'
+                  className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300'>
+                  Read more
+                </a>
+              </div>
+            </div>
           </div>
+          {/* end */}
         </div>
       </div>
     </div>
