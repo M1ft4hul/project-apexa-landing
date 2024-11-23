cd5a69ca593d93c050bb9f469c4770ebb9e5d1af
Abd. Majid Hamid <abd.majidehamide@gmail.com>
2024-11-23 13:22:07 +0800

    FOOTER 95% PROGRESS

--- app/componen/footer/Footer.tsx
+++ app/componen/footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className='bg-[#59859D] relative '>
         <div className='container mx-auto lg:max-w-screen-xl md:max-w-screen-md lg:px-0 px-0'>
           <div className='footer_2 border-b border-dark_border xl:px-0 px-6'>
-            <div className='grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-3 lg:gap-0 gap-8 grid-cols-2'>
+            <div className='grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 lg:gap-0 gap-8 grid-cols-2'>
               {/* logo */}
               <div>
                 <div className='flex gap-3 items-center mb-6'>
@@ -168,7 +168,7 @@ const Footer = () => {
                 </ul>
               </div>
               {/* Apakah kamu ada pertanyaan? */}
-              <div className='ml-8'>
+              <div className='sm:col-span-2 ml-8'>
                 <div className='flex gap-3 items-center mb-6'>
                   <p className='text-white text-2xl	font-normal mb-4 leading-loose'>
                     Apakah kamu ada pertanyaan?
@@ -185,76 +185,6 @@ const Footer = () => {
                 <h4 className='text-white text-base font-medium mb-4'>
                   Get Social
                 </h4>
-                <div className='flex gap-6'>
-                  <a className='text-grey hover:text-white' href='#'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      xmlnsXlink='http://www.w3.org/1999/xlink'
-                      aria-hidden='true'
-                      role='img'
-                      className='iconify iconify--fa6-brands'
-                      width={24}
-                      height={24}
-                      viewBox='0 0 448 512'
-                      style={{ verticalAlign: "-0.125em" }}>
-                      <path
-                        fill='currentColor'
-                        d='M64 32C28.7 32 0 60.7 0 96v320c0 35.3 28.7 64 64 64h320c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64zm297.1 84L257.3 234.6L379.4 396h-95.6L209 298.1L123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5l78.2-89.5zm-37.8 251.6L153.4 142.9h-28.3l171.8 224.7h26.3z'
-                      />
-                    </svg>
-                  </a>
-                  <a className='text-grey hover:text-white' href='#'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      xmlnsXlink='http://www.w3.org/1999/xlink'
-                      aria-hidden='true'
-                      role='img'
-                      className='iconify iconify--fa6-brands'
-                      width={24}
-                      height={24}
-                      viewBox='0 0 448 512'
-                      style={{ verticalAlign: "-0.125em" }}>
-                      <path
-                        fill='currentColor'
-                        d='M64 32C28.7 32 0 60.7 0 96v320c0 35.3 28.7 64 64 64h98.2V334.2h-52.8V256h52.8v-33.7c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480h129c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64z'
-                      />
-                    </svg>
-                  </a>
-                  <a className='text-grey hover:text-white' href='#'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      xmlnsXlink='http://www.w3.org/1999/xlink'
-                      aria-hidden='true'
-                      role='img'
-                      className='iconify iconify--fa6-brands'
-                      width={24}
-                      height={24}
-                      viewBox='0 0 448 512'
-                      style={{ verticalAlign: "-0.125em" }}>
-                      <path
-                        fill='currentColor'
-                        d='M194.4 211.7a53.3 53.3 0 1 0 59.3 88.7a53.3 53.3 0 1 0-59.3-88.7m142.3-68.4c-5.2-5.2-11.5-9.3-18.4-12c-18.1-7.1-57.6-6.8-83.1-6.5c-4.1 0-7.9.1-11.2.1s-7.2 0-11.4-.1c-25.5-.3-64.8-.7-82.9 6.5c-6.9 2.7-13.1 6.8-18.4 12s-9.3 11.5-12 18.4c-7.1 18.1-6.7 57.7-6.5 83.2c0 4.1.1 7.9.1 11.1s0 7-.1 11.1c-.2 25.5-.6 65.1 6.5 83.2c2.7 6.9 6.8 13.1 12 18.4s11.5 9.3 18.4 12c18.1 7.1 57.6 6.8 83.1 6.5c4.1 0 7.9-.1 11.2-.1s7.2 0 11.4.1c25.5.3 64.8.7 82.9-6.5c6.9-2.7 13.1-6.8 18.4-12s9.3-11.5 12-18.4c7.2-18 6.8-57.4 6.5-83c0-4.2-.1-8.1-.1-11.4s0-7.1.1-11.4c.3-25.5.7-64.9-6.5-83c-2.7-6.9-6.8-13.1-12-18.4zm-67.1 44.5a82 82 0 1 1-91.2 136.4a82 82 0 1 1 91.1-136.4zm29.2-1.3c-3.1-2.1-5.6-5.1-7.1-8.6s-1.8-7.3-1.1-11.1s2.6-7.1 5.2-9.8s6.1-4.5 9.8-5.2s7.6-.4 11.1 1.1s6.5 3.9 8.6 7s3.2 6.8 3.2 10.6c0 2.5-.5 5-1.4 7.3s-2.4 4.4-4.1 6.2s-3.9 3.2-6.2 4.2s-4.8 1.5-7.3 1.5c-3.8 0-7.5-1.1-10.6-3.2zM448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96v320c0 35.3 28.7 64 64 64h320c35.3 0 64-28.7 64-64zm-91 293c-18.7 18.7-41.4 24.6-67 25.9c-26.4 1.5-105.6 1.5-132 0c-25.6-1.3-48.3-7.2-67-25.9s-24.6-41.4-25.8-67c-1.5-26.4-1.5-105.6 0-132c1.3-25.6 7.1-48.3 25.8-67s41.5-24.6 67-25.8c26.4-1.5 105.6-1.5 132 0c25.6 1.3 48.3 7.1 67 25.8s24.6 41.4 25.8 67c1.5 26.3 1.5 105.4 0 131.9c-1.3 25.6-7.1 48.3-25.8 67z'
-                      />
-                    </svg>
-                  </a>
-                  <a className='text-grey hover:text-white' href='#'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      xmlnsXlink='http://www.w3.org/1999/xlink'
-                      aria-hidden='true'
-                      role='img'
-                      className='iconify iconify--fa6-brands'
-                      width={24}
-                      height={24}
-                      viewBox='0 0 576 512'
-                      style={{ verticalAlign: "-0.125em" }}>
-                      <path
-                        fill='currentColor'
-                        d='M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597c-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821c11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305m-317.51 213.508V175.185l142.739 81.205z'
-                      />
-                    </svg>
-                  </a>
-                </div>
               </div>
             </div>
           </div>
